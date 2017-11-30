304987f650536ae27fe36bf93c8df3dc277b5ed2
Chiran <chirud111@gmail.com>
2017-11-30 16:05:54 -0600

    added new functionality 'addTask' and used ES6 arrow functions

--- todolist.js
+++ todolist.js
@@ -1,20 +1,37 @@
 
 
-function addItems() {
+var addTask = () =>{
+  var taskName = document.getElementById('task').value;
+  var wrapper= document.createElement('div');
+  wrapper.innerHTML = '<div class="checkbox">\
+        <label><input type="checkbox" class="tasksCheckbox" value =' + taskName +' >'+ taskName +' </label>\
+    </div>';
+    var div= wrapper.firstChild;
+
+  document.getElementById('beforecomplete').appendChild(div);
+  document.getElementById('task').value = "";
+}
+
+
+
+
+var addItems = () => {
   var checkedItems = document.querySelectorAll('.tasksCheckbox:checked');
+
   var completedTemplate = '';
   for(var i =0;i<checkedItems.length;i++) {
-    
-        var result = (checkedItems[i].value).strike();
+  checkedItems[i].parentNode.style.textDecoration = 'line-through';
+  checkedItems[i].disabled = true;
     completedTemplate = completedTemplate + '' + '<li>'+checkedItems[i].value +'</li>';
 
   }
+
   document.getElementById('completed').innerHTML = completedTemplate;
 }
 
 
 
-function deleteItems(){
+var deleteItems = () =>{
 
   var checkedItems = document.querySelectorAll('.tasksCheckbox:checked');
 
